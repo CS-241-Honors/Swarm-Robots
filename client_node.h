7c4663fa81b2dfbe25cb647d928e1947c1da6691
MINGCHAO ZHANG <mingchao@wirelessprv-10-193-6-208.near.illinois.edu>
2018-10-26 00:40:35 -0500

    add head param

--- client_node.h
+++ client_node.h
@@ -10,8 +10,8 @@ typedef struct client {
 } client;
 
 //return 0 upon success, -1 otherwise
-int insert_client(client * head, client * new_node);
-int create_client(client * head, char * ip, long int port, char * client_name);
-void delete_client(client * head, char * ip, long int port);
-void delete_all_client(client * head);
+int insert_client(client ** head, client * new_node);
+int create_client(client ** head, char * ip, long int port, char * client_name);
+void delete_client(client ** head, char * ip, long int port);
+void delete_all_client(client ** head);
 

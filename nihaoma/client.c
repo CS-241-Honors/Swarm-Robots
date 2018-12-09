7d84f89c4ac69dc33abff1a3fc2a48b68b619220
JOSEPH ESPINO <espino2@illinois.edu>
2018-12-08 22:58:42 -0600

    message responding implemented

--- nihaoma/client.c
+++ nihaoma/client.c
@@ -29,12 +29,17 @@
 #define CPORT "9002"
 #define DPORT "9003"
 
+
 //----------------------------------------------------------------------------------
 // Global variables
 static char my_name;
 static char * my_port;
 static dictionary * table;
 static vector * seek_ports;
+static int population_size = 4;
+static char population[4] = {'A', 'B', 'C', 'D'};
+static int messages_sent; 
+
 
 //----------------------------------------------------------------------------------
 // Structures
@@ -51,10 +56,194 @@ typedef struct message{
     int id;
     char to;
     char from;
-    int type;
-    int subtype;
-} message; 
+    int type; // 1, que
+    int subtype; // 1, 2, 3, 4,
+    char request_name;
+} message;
 
+enum { 
+    QUERY,
+    RESPONSE,
+    MOVE,
+    DISCONNECT
+};
+
+//----------------------------------------------------------------------------------
+
+
+void send_to_all_neighbors(void * package){
+    (void)package;
+
+    for (int i = 0; i < population_size; i++){
+        if(dictionary_contains(table, (void *)(size_t)population[i])){
+            
+            value * curr_value = dictionary_get(table, (void *)(size_t)(population[i]));
+            int curr_fd = curr_value->fd;
+            printf("Sending query package to %c on fd #%d\n", population[i], curr_fd);
+            write(curr_fd, package, sizeof(message));
+        }
+    }
+
+    sleep(1);
+
+}
+
+//----------------------------------------------------------------------------------
+// int get_fd_from_table(char key){
+
+//     printf("KEY OUT : %c\n", key);
+//     value * value_ = (value *) dictionary_get(table, (void *) (size_t) key);
+
+//     return value_->fd;
+
+// }
+
+//----------------------------------------------------------------------------------
+/**
+ * TODO: implement the return values
+ * returns 1 on success
+ * returns -1 on failure
+ */
+int send_message(char to, int type, int subtype, char request_name){
+    message * package = (message *) malloc(sizeof(message));
+    package->id = messages_sent;
+    messages_sent += 1;
+    package->to = to;
+    package->from = my_name;
+    package->type = type;
+    package->subtype = subtype;
+    package->request_name = request_name;
+
+    // int fd = get_fd_from_table(to);
+
+    // printf("File descriptor recipient : %d\n", fd);
+    // (void)fd;
+    // char tmp_buf[sizeof(message)];
+
+    printf("MESSAGE :\n");
+    printf("package->id : %d\n", package->id);
+    printf("package->to : %c\n", package->to);
+    printf("package->from : %c\n", package->from);
+    printf("package->type : %d\n", package->type);
+    printf("package->subtype : %d\n", package->subtype);
+    printf("package->request_name : %d\n", package->request_name);
+
+
+    // write(fd, package, sizeof(message));
+    if (to == 'Z'){
+        send_to_all_neighbors(package);
+    }else{
+        value * curr_value = dictionary_get(table, (void *)(size_t)(to));
+        int curr_fd = curr_value->fd;
+
+        printf("Sending a singular message to %c on fd #%d\n", to, curr_fd);
+        write(curr_fd, package, sizeof(message));
+    }
+
+    return 0;
+
+}
+
+
+//----------------------------------------------------------------------------------
+/**
+ * read from handler
+ * 
+ */
+
+void message_interpreter(char messenger_name, int messenger_fd, message * package){
+
+    if (package->type == QUERY){
+        printf("We have a query for %c\n", package->request_name);
+        send_message(messenger_name, RESPONSE, 1, package->request_name);
+
+    }else if (package->type == RESPONSE){
+        printf("Response message received\n");
+    }
+
+}
+
+//----------------------------------------------------------------------------------
+/**
+ * read from handler
+ * 
+ */
+
+void * read_neighbor_handler(void * name){
+    char neighbor_name = (char)(size_t)name;
+    printf("Now reading from neighbor %c...\n", neighbor_name);
+    value * neighbor_value = (value *) dictionary_get(table, name);
+    int fd = neighbor_value->fd;
+    while(1){
+
+        // 
+        char message_buffer[sizeof(message)];
+        printf("Reading from neighbor %c on fd %d...\n", neighbor_name, fd);
+        int bytes_read = read(fd, message_buffer, sizeof(message));
+        (void)bytes_read;
+        printf("Received message from : %c\n", ((message *)message_buffer)->from);
+
+        message * new_package = (message *) message_buffer;
+
+        message_interpreter(neighbor_name, fd, new_package);
+
+        sleep(1);
+
+    }
+}
+
+//----------------------------------------------------------------------------------
+/**
+ * nnwrite == new neighbor write
+ * 
+ */
+
+void nnwrite(int sock_fd){
+
+
+    char mesg_buffer[sizeof(message)];
+    message * mesg_ = (message *) mesg_buffer;
+    mesg_->from = my_name;
+    printf("Inside nnwrite(), sending message...\n");
+    write(sock_fd, mesg_buffer, sizeof(message));
+
+
+}
+
+//----------------------------------------------------------------------------------
+/**
+ * nnread == new neighbor read
+ * 
+ */
+
+void nnread(int new_neighbor_fd){
+
+    char init_buffer[sizeof(message)];    
+    read(new_neighbor_fd, init_buffer, sizeof(message));
+    message * message_data = (message *)init_buffer;
+    char new_neighbor_name = message_data->from;
+    // fill out value
+    value * new_value = (value *) malloc(sizeof(value));
+    new_value->fd = new_neighbor_fd;
+    new_value->direct = 1;
+    // throw it in table
+
+    printf("Attempting to add to our dictionary.....\n");
+    if(!dictionary_contains(table, (void *)(size_t)new_neighbor_name)){
+
+        dictionary_set(table, (void *)(size_t)new_neighbor_name, (void *)new_value);
+        printf("Robot %c is now added to my dictionary and has fd #%d\n", new_neighbor_name, new_neighbor_fd);
+
+        pthread_t read_from_thread;
+        if(pthread_create(&read_from_thread, NULL, read_neighbor_handler, (void *)(size_t)new_neighbor_name)){
+            perror("pthread_create() error");
+            exit(1);
+        }
+
+    }else{
+        printf("Not adding to our dictionary, already present\n");
+    }
+}
 
 //----------------------------------------------------------------------------------
 /**
@@ -86,6 +275,9 @@ void populate_seek_ports(){
 
 }
 
+
+
+
 //----------------------------------------------------------------------------------
 /**
  * get port
@@ -112,6 +304,36 @@ char * get_port(){
 
     return my_port;
 }
+
+//----------------------------------------------------------------------------------
+/**
+ * fill out the rest of the table
+ * 
+ */
+
+void * table_handler(){
+
+    int counter = 0;
+    while(1){
+        char current_neighbor = population[counter%population_size];
+        if (!dictionary_contains(table, (void *)(size_t)current_neighbor)){
+            if (current_neighbor != my_name){
+                printf("NEED TO CONNECT TO : %c\n", current_neighbor);
+                send_message('Z', QUERY, 0, current_neighbor);
+                sleep(1);
+            }
+        }
+        counter++;
+    }
+
+
+
+    return NULL;
+}
+
+
+
+
 
 //----------------------------------------------------------------------------------
 /**
@@ -153,20 +375,14 @@ void * open_ears(void * ignore){
         printf("Now accepting...\n");
         int new_neighbor_fd = accept(ear_fd, NULL, NULL);
         // read the init message
-        char init_buffer[sizeof(message)];
+        
         printf("Now reading from new neighbor...\n");
-        read(new_neighbor_fd, init_buffer, sizeof(message));
-        message * message_data = (message *)init_buffer;
-        char new_neighbor_name = message_data->from;
-        // fill out value
-        value * new_value = (value *) malloc(sizeof(value));
-        new_value->fd = new_neighbor_fd;
-        new_value->direct = 1;
-        // throw it in table
-        dictionary_set(table, (void *)(size_t)new_neighbor_name, (void *)new_value);
 
-        printf("Robot %c is now added to my dictionary!\n", new_neighbor_name);
+        nnwrite(new_neighbor_fd);
+        nnread(new_neighbor_fd);
+        
     }
+
 
     return NULL;
 }
@@ -179,7 +395,7 @@ void * open_ears(void * ignore){
  void connect_to(char * host, char * port){
     struct addrinfo hints, *result;
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_INET;
+    hints.ai_family = AF_INET;                            
     hints.ai_socktype = SOCK_STREAM;
 
     int s = getaddrinfo(host, port, &hints, &result);
@@ -201,24 +417,12 @@ void * open_ears(void * ignore){
     }
 
 
-    char init_buffer[sizeof(message)];
-    printf("Now reading from new neighbor...\n");
-    read(sock_fd, init_buffer, sizeof(message));
-    message * message_data = (message *)init_buffer;
-    char new_neighbor_name = message_data->from;
-    // fill out value
-    value * new_value = (value *) malloc(sizeof(value));
-    new_value->fd = sock_fd;
-    new_value->direct = 1;
-    // throw it in table
-    if (dictionary_contains(table, (void *)(size_t)new_neighbor_name)){
-        return 'Z';
-    }
-    dictionary_set(table, (void *)(size_t)new_neighbor_name, (void *)new_value);
-    printf("Robot %c is now added to my dictionary!\n", new_neighbor_name);
+    nnwrite(sock_fd);
+    nnread(sock_fd);
 
 
-    return 
+
+    return;
  }
 
 //----------------------------------------------------------------------------------
@@ -232,9 +436,12 @@ void * seek_out(void * ignore){
 
 
     for(size_t i = 0; i < vector_size(seek_ports); i++){
-        char possibly_new_neighbor = connect_to(LOCAL_IP, vector_get(seek_ports, i));
-        if (dictionary(s))
+        connect_to(LOCAL_IP, vector_get(seek_ports, i));
+        
     }
+
+    printf("Done seeking pre-defined neighbors\n");
+
 
     return NULL;
 }
@@ -270,7 +477,7 @@ int main(int argc, char * argv[]){
         exit(1);
     }
 
-    sleep(10);
+    sleep(5);
 
 
     pthread_t seek_out_thread;
@@ -278,6 +485,16 @@ int main(int argc, char * argv[]){
         perror("pthread_create() error");
         exit(1);
     }
+
+    sleep(4);
+
+    pthread_t table_thread;
+     if(pthread_create(&table_thread, NULL, table_handler, NULL)){
+        perror("pthread_create() error");
+        exit(1);
+    }
+
+
 
 
     pthread_exit(NULL);
@@ -287,5 +504,14 @@ int main(int argc, char * argv[]){
 }
 
 
+//----------------------------------------------------------------------------------
+/**
+ *  EXTRA
+ * 
+ */
 
+// while(1){
+        //     printf("Reading from neigbor %c\n", neighbor_name);
+        //     sleep(2);
+        // }
 
